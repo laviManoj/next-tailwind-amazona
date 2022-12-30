28871807c47c326da2c45421fcc4644da0125797
laviManoj <lavigeremanoj@gmail.com>
2022-12-31 00:49:46 +0530

    Added Header and Main Section and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,35 +1,26 @@
 import React, { Children } from "react";
 import Head from "next/head";
+import Link from "next/head"
 
 export default function Layout({ children }) {
     return (
         <>
-            <Head>
-                <title>Amazon</title>
+         <Head>
+                <title>Amazon </title>
                 <meta name="description" content="Ecommerce Website" />
-                <link rel="icon" href="/favicon.ico" />
+                <Link legacyBehavior rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="flex min-h-screen flex-cool justify-between">
+            <div className="flex min-h-screen flex-col justify-between">
                 <header>
-                    <nav className="flex h-12 items-center px-4 justify-between  shadow-md">
-                        <Link href="">
-                            <a className="text-lg font-bold">amazona</a>
-                        </Link>
-                        <div>
-                            <Link href="/cart">
-                                <a className="p-2">Cart</a>
-                            </Link>
-                            <Link href="/login">
-                                <a className="p-2">Login</a>
-                            </Link>
-                        </div>
-                    </nav>
+                    header
                 </header>
-                <main className="container m-auto mt-4 px-4">{children}</main>
-                <footer className="flex h-10 justify-center items-center shadow-inner">
-                    <p> Copyright @2022 Amazona</p>
+                <main>
+                    {children}
+                </main>
+                <footer>
+                    footer
                 </footer>
             </div>
         </>
-    );
+    )
 }
