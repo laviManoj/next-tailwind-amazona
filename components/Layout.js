013ebc470b2694cd451dca9532d2e99998bb5f76
laviManoj <lavigeremanoj@gmail.com>
2022-12-31 01:18:10 +0530

    Added Cart and login Details

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,9 @@
 import React, { Children } from "react";
 import Head from "next/head";
-import Link from "next/head"
+import Link from 'next/link'
+
+
+
 
 export default function Layout({ children }) {
     return (
@@ -8,17 +11,29 @@ export default function Layout({ children }) {
          <Head>
                 <title>Amazon </title>
                 <meta name="description" content="Ecommerce Website" />
-                <Link legacyBehavior rel="icon" href="/favicon.ico" />
+                <a  rel="icon" href="/favicon.ico"  />
             </Head>
+
+
             <div className="flex min-h-screen flex-col justify-between">
                 <header>
-                    header
+                  <nav className="flex h-12 items-center px-4 justify-between  shadow-md">
+                    <a  href="/" >
+                        <a className="text-lg font-bold">amazon</a>
+                    </a>
+                    <div>
+                    <a  href="/cart" >
+                                <a className="p-2">Cart</a>
+                            </a>
+                            <a  href="/login" >
+                                <a className="p-2">Login</a>
+                            </a>
+                    </div>
+                 </nav>
                 </header>
-                <main>
-                    {children}
-                </main>
-                <footer>
-                    footer
+                <main className="container m-auto mt-4 px-4">{children}</main> 
+                <footer className="flex h-10 justify-center items-center shadow-inner ">
+                    <p> Copyright @2022 Amazona</p>
                 </footer>
             </div>
         </>
