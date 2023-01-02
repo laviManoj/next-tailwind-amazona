ff547f3f99c828a7099ca5591e80e614617d2787
laviManoj <lavigeremanoj@gmail.com>
2023-01-02 21:42:27 +0530

    Added Code ProductList

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,29 +1,31 @@
-import React from 'react'
-// import a from 'next/link'
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import React from 'react';
 
 export default function ProductItem({ product }) {
-    return (
-        <div className="card">
-            {/* //link tag replace the a  */}
-            <a href={`/product/${product.slug}`}>
-                <a>
-                    <img src={product.image} alt={product.name} className="rounded shadow" />
-                </a>
-            </a>
-            <div className="flex flex-col items-center justify-center p-5">
-            <a href={`/product/${product.slug}`}>
-                <a>
-            <h2 className='text-lg'>
-                    {product.name}
-                </h2>
-                </a>
-                </a>
-               <p className='mb-2'>{product.brand}</p>
-               <p>${product.price}</p>
-               <button className="primary-button" type="button">
-                Add to cart
-               </button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <Link  legacyBehavior href={`/product/${product.slug}`}>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link legacyBehavior href={`/product/${product.slug}`}>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
 }
